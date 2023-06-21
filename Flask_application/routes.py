852bc85d7f42f65e1fa83f3b2cf0db0981bdc8e0
Mattjben <s3923076@student.rmit.edu.au>
2023-06-21 18:48:49 +1000

    a

--- Flask_application/routes.py
+++ Flask_application/routes.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, redirect, url_for,flash,session
 from Flask_application import app
 import pandas as pd
+import os
 
 #------------------------------------------------------------------------------------
 # HomePage - website info 
@@ -9,7 +10,9 @@ import pandas as pd
 @app.route('/')
 @app.route('/Home')
 def root_route():
-   exampletable = pd.read_excel(r'Flask_application\static\Sampletable.xlsx')
+   current_directory = os.path.dirname(__file__)
+   file_path = os.path.join(current_directory, "static", "Sampletable.xlsx")
+   exampletable = pd.read_excel(file_path)
    table_html = exampletable.to_html(table_id="table", classes='table table-light table-striped-columns')
    return render_template('Home.html',title="Travel Data Input",table=table_html)
 
