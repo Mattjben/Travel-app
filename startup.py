3d3ba905a05eb0930a104db52d402e1afca919d5
Mattjben <s3923076@student.rmit.edu.au>
2023-06-21 17:58:50 +1000

    a

--- startup.py
+++ startup.py
@@ -1,4 +1,5 @@
+#!/usr/bin/env python3
 from flask import Flask 
 from Flask_application import app
 if __name__ == '__main__':
-    app.run(debug=True)
+      app.run(host='0.0.0.0',port="8400",debug=True)
